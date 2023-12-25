a2f4b6efd56c98875df92d4ba3ef909cde0b3c66
Eddie <65310217+Eddiek1102@users.noreply.github.com>
2023-12-25 15:40:39 -0500

    Update 2_print_linked_list.py

--- Singly Linked List/2_print_linked_list.py
+++ Singly Linked List/2_print_linked_list.py
@@ -6,18 +6,22 @@ class Node:
 class LinkedList: 
     def __init__(self, value): 
         new_node = Node(value)
-        self.next = None 
         self.head = new_node 
         self.tail = new_node 
         self.length = 1 
 
-    #print contents of linked list 
+    '''
+        this function prints the values of all the nodes in the linked list. 
+        a temproary variable 'temp' is set to the head of the linked list. 
+        a while loop checks it the temporary variable is set to None (reached end of the list?) 
+        iterates through the list and prints the values of each node and then moves the temporary variable to the next node in the list. 
+    '''
     def print_list(self): 
-        temp = self.head #start at head node
-        while temp != None: #check if node's next pointer is pointing to None
-            print(temp.value) #print value of current node 
-            temp = temp.next #iterate to next node 
+        temp = self.head  
+        while temp != None:  
+            print(temp.value)  
+            temp = temp.next 
 
 if __name__ == "__main__": 
     my_linked_list = LinkedList("Hello World") 
-    my_linked_list.print_list() #prints "Hello World" 
+    my_linked_list.print_list() 
